759bfd8e3c353f8b11a4998e5fdcbe348db95df4
Miguel Pou <miguelapou@gmail.com>
2025-11-29 13:27:45 -0500

    Update TakumiGarage.js

--- components/TakumiGarage.js
+++ components/TakumiGarage.js
@@ -4724,7 +4724,7 @@ const TakumiGarage = () => {
         <div className="flex flex-col md:grid md:grid-cols-2 lg:grid-cols-[2fr_1fr] gap-6 mb-6">
           {/* Statistics Cards - order-1 on mobile, contains search on desktop */}
           <div className="space-y-4 order-1 md:order-none">
-            <div className="grid grid-cols-2 md:grid-cols-4 gap-3 sm:gap-4">
+            <div className="grid grid-cols-2 lg:grid-cols-4 gap-3 sm:gap-4">
               <div 
                 onClick={() => {
                   setStatusFilter(statusFilter === 'purchased' ? 'all' : 'purchased');
