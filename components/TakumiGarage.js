34cef05672667b51bdec9a8e034c29c89cf67216
Miguel Pou <miguelapou@gmail.com>
2025-11-30 09:29:53 -0500

    Update TakumiGarage.js

--- components/TakumiGarage.js
+++ components/TakumiGarage.js
@@ -6773,7 +6773,7 @@ const TakumiGarage = () => {
                     >
                       {/* Drag Handle - Hidden on mobile */}
                       <div 
-                        draggable
+                        draggable="true"
                         onDragStart={(e) => {
                           e.stopPropagation();
                           handleVehicleDragStart(e, vehicle);
@@ -6783,7 +6783,7 @@ const TakumiGarage = () => {
                           }
                         }}
                         onDragEnd={handleVehicleDragEnd}
-                        className={`absolute top-2 left-2 cursor-grab active:cursor-grabbing hidden md:block z-10 ${
+                        className={`absolute top-2 left-2 cursor-grab active:cursor-grabbing hidden md:block z-10 pointer-events-auto ${
                           darkMode ? 'text-gray-400 hover:text-gray-300' : 'text-gray-500 hover:text-gray-700'
                         }`}
                       >
